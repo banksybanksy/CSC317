69a1f4f59e8df7de226b237d768c17816a1a8033
Damian Perez <damedash03@gmail.com>
2025-05-12 11:18:46 -0700

    refactor: removed all redundant files, only index.html remains.

--- assignments/final/routes/watchlist.js
+++
@@ -1,9 +0,0 @@
-/**
- * watchlist.js
- * 04-29-2025- Modified by Cielina Lubrino
- */
-
-router.get('/dashboard', async (req, res) => {
-    const user = await User.findById(req.session.user._id).populate('watchlist');
-    res.render('user/dashboard', { user });
-});
